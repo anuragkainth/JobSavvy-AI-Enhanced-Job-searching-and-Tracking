cbe08687cd770aa778c634f1410c19063d0fdd05
anuragkainth <anuragkainth07@gmail.com>
2024-09-29 20:32:17 +0530

    fix: firebase config fix added

--- home.py
+++ home.py
@@ -142,6 +142,16 @@ def trigger_alert(message):
 def main():
     
     #Initialize Firebase
+    firebaseConfig = {
+        "apiKey": os.getenv("FIREBASE_API_KEY"),
+        "authDomain": os.getenv("FIREBASE_AUTH_DOMAIN"),
+        "databaseURL": os.getenv("FIREBASE_DATABASE_URL"),
+        "projectId": os.getenv("FIREBASE_PROJECT_ID"),
+        "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET"),
+        "messagingSenderId": os.getenv("FIREBASE_MESSAGING_SENDER_ID"),
+        "appId": os.getenv("FIREBASE_APP_ID"),
+        "measurementId": os.getenv("FIREBASE_MEASUREMENT_ID")
+    }
     firebase = pyrebase.initialize_app(firebaseConfig)
     auth = firebase.auth()
     db = firebase.database()
